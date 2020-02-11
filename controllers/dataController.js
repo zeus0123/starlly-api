00946cd696fd0bc2f2fe2cfd548a8093bf76664e
Trishan Ghosh <ghoshtrishan73@gmail.com>
2020-02-11 22:34:18 +0530

    change in dataRoutes

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,4 +1,4 @@
-const csvData = require('../model/CsvData');
+const CsvData = require('../model/CsvData');
 const path = require('path');
 const csv = require('csv-parser');
 const fs = require('fs');
@@ -26,7 +26,7 @@ let data = [];
 
    .on('end', () => {
        console.log('finish');
-       
+        res.end();
         saveToDB(data);
 
    })
@@ -39,8 +39,46 @@ let data = [];
 
 const saveToDB = (data) => {
     data.forEach((element,index) => {
-        setTimeout(() => {
-            console.log(element.Parameter1);
+        setTimeout(async () => {
+            const insertData = await CsvData.query().insert({
+                parameter_one : element.Parameter1,
+                parameter_two : element.Parameter2,
+                parameter_three : element.Parameter3,
+                parameter_five : element.Parameter5
+            })
         },index*3000) 
     });
+}
+
+exports.getData =  (req,res) => {
+      
+    CsvData.query().then(results => {
+        return res.status(200).send({
+            code : 1,
+            data : results
+        });
+    }).catch(err => {
+        res.status(500).send({
+            code: 0,
+            message : 'Server Issue'
+        })
+    })
+    
+
+    
+}
+
+exports.getLatestRow = (req,res) => {
+    CsvData.query().orderBy('created_at','desc').limit(1)
+        .then(result => {
+            return res.status(200).send({
+                code: 1,
+                data : result
+            })
+        }).catch(err => {
+            res.status(500).send({
+                code : 0,
+                message : 'Server Issue'
+            })
+        })
 }
--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const dataController = require('../controllers/dataController');
 
 router.get('/data-analysis',dataController.dataAnalysis);
+router.get('/get-data',dataController.getData);
+router.get('/latest-data',dataController.getLatestRow);
 
 module.exports = router;
